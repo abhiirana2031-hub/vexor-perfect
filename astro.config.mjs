fa8019d500a3bb4e8d0812567b636f9815a636ff
Abhay Rana <abhayrana8272@gmail.com>
2026-03-26 16:32:36 +0530

    Revert "deploy: switch to Vercel adapter"
    This reverts commit 07e5091dfbccddabc77e2c6ef10426d68a9adeec.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import customErrorOverlayPlugin from "./vite-error-overlay-plugin.js";
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -64,5 +64,7 @@ export default defineConfig({
     checkOrigin: false
   },
 
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });
